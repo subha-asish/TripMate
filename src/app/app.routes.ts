cc3d059000883e7e35730b669a7dee0a38011912
subhasis pradhan <subhasishpradhan807@gmail.com>
2024-11-19 23:13:44 +0530

    TripMate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { CustomerMgmtComponent } from './customer-mgmt/customer-mgmt.component';
+import { TourMgmtComponent } from './tour-mgmt/tour-mgmt.component';
+import { BookingMgmtComponent } from './booking-mgmt/booking-mgmt.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'customers',
+        component: CustomerMgmtComponent
+    },
+    {
+        path: 'tours',
+        component: TourMgmtComponent
+    },
+    {
+        path: 'bookings',
+        component: BookingMgmtComponent
+    }
+];
